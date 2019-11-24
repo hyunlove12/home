df33b56d704b1f7066ed067de833ab71972204f7
dh <hyunlove12@naver.com>
2019-11-25 00:19:11 +0900

    first

---
+++ src/main/java/dh/com/est/login/service/LoginService.java
@@ -0,0 +1,9 @@
+package dh.com.est.login.service;
+
+import java.util.ArrayList;
+
+public interface LoginService {
+	
+	public ArrayList<LoginVO> userList();
+	
+}
---
+++ src/main/java/dh/com/est/login/serviceImpl/LoginDaoInter.java
@@ -0,0 +1,11 @@
+package dh.com.est.login.serviceImpl;
+
+import java.util.ArrayList;
+
+import dh.com.est.login.service.LoginVO;
+
+public interface LoginDaoInter {
+
+	public ArrayList<LoginVO> userList();
+	
+}
